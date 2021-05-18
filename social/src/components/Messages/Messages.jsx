9546cc7eeb24dd2db81de34fc0a6feab1bee02c4
heikun <heikun@yandex.ru>
2021-05-18 20:31:26 +0300

    Array map content

--- social/src/components/Messages/Messages.jsx
+++ social/src/components/Messages/Messages.jsx
@@ -1,7 +1,58 @@
 import React from "react";
 import style from './Messages.module.css';
 import {NavLink} from "react-router-dom";
-import {BrowserRouter, Route} from "react-router-dom";
+import {Route} from "react-router-dom";
+
+let MessagesUsers = [
+    {
+        userId: 1,
+        userImage: 'https://st.fl.ru/users/na/nadezhdaNade/foto/f_1455b7479a7e07ac.jpg',
+        userName: 'Diana',
+        lastMessage: 'Hi Alex! It’s Diana, I just wanted to let you know that we have to reschedule',
+        lastMessageDate: '4 hours ago'
+    },
+    {
+        userId: 2,
+        userImage: 'https://st.fl.ru/users/ka/kate-seo/foto/f_3726045b60cd904f.jpeg',
+        userName: 'Julia',
+        lastMessage: 'Hi! Can you help me?',
+        lastMessageDate: '1 day ago'
+    },
+    {
+        userId: 3,
+        userImage: 'https://st.fl.ru/users/Mi/Mihail_Kachan/foto/f_8095d9e1685c17b9.jpg',
+        userName: 'Michail',
+        lastMessage: 'Hello man! I can\'t get YouTube play for React',
+        lastMessageDate: '2 days ago'
+    },
+]
+
+let MessagesArray = [
+    {
+        messageId: 1,
+        userImage: "https://st.fl.ru/users/na/nadezhdaNade/foto/f_1455b7479a7e07ac.jpg",
+        textMessage: 'Hi James! It’s Diana, I just wanted to let you know that we have to reschedule',
+        messageStyle: style.message_for_me
+    },
+    {
+        messageId: 2,
+        userImage: "https://st.fl.ru/users/he/heikun/foto/f_293609fee65839c2.jpeg",
+        textMessage: 'Hi Diana! How are you?',
+        messageStyle: style.message_for_people
+    },
+    {
+        messageId: 3,
+        userImage: "https://st.fl.ru/users/na/nadezhdaNade/foto/f_1455b7479a7e07ac.jpg",
+        textMessage: 'Alex, can you call me at Friday?',
+        messageStyle: style.message_for_me
+    },
+    {
+        messageId: 4,
+        userImage: "https://st.fl.ru/users/he/heikun/foto/f_293609fee65839c2.jpeg",
+        textMessage: 'Yeah, I can call you at Friday on 6-7 pm',
+        messageStyle: style.message_for_people
+    },
+]
 
 const MessagesUser = (props) => {
     return (
@@ -20,43 +71,29 @@ const MessagesUser = (props) => {
     )
 }
 
+const MessageItem = (props) => {
+    return (
+        <div className={`${style.message_in_dialog} ${props.messageStyle}`}>
+            <div className={style.author_thumb}>
+                <img src={props.userImage} alt="author" />
+            </div>
+            <p>{props.textMessage}</p>
+        </div>
+    )
+}
+
 const Messages = () => {
     return (
         <div className={style.container}>
             <div className={style.content}>
                 <div className={style.list_messages}>
                     <div className={`${style.notification_list} ${style.chat_message}`}>
-                        <MessagesUser userId="1" userImage="https://st.fl.ru/users/na/nadezhdaNade/foto/f_1455b7479a7e07ac.jpg" userName="Diana" lastMessage="Hi Alex! It’s Diana, I just wanted to let you know that we have to reschedule" lastMessageDate="4 hours ago" />
-                        <MessagesUser userId="2" userImage="https://st.fl.ru/users/ka/kate-seo/foto/f_3726045b60cd904f.jpeg" userName="Julia" lastMessage="Hi! Can you help me?" lastMessageDate="1 day ago" />
-                        <MessagesUser userId="3" userImage="https://st.fl.ru/users/Mi/Mihail_Kachan/foto/f_8095d9e1685c17b9.jpg" userName="Michail" lastMessage="Hello man! I can't get YouTube play for React" lastMessageDate="2 days ago" />
+                        {MessagesUsers.map( user => <MessagesUser userId={user.userId} userImage={user.userImage} userName={user.userName} lastMessage={user.lastMessage} lastMessageDate={user.lastMessageDate} /> )}
                     </div>
                 </div>
                 <div className={style.now_dialogs}>
                     <Route path="/messages/1">
-                        <div className={`${style.message_in_dialog} ${style.message_for_me}`}>
-                            <div className={style.author_thumb}>
-                                <img src="https://st.fl.ru/users/he/heikun/foto/f_293609fee65839c2.jpeg" alt="author" />
-                            </div>
-                            <p>Hi James! It’s Diana, I just wanted to let you know that we have to reschedule</p>
-                        </div>
-                        <div className={`${style.message_in_dialog} ${style.message_for_people}`}>
-                            <div className={style.author_thumb}>
-                                <img src="https://st.fl.ru/users/he/heikun/foto/f_293609fee65839c2.jpeg" alt="author" />
-                            </div>
-                            <p>Hi Diana! How are you?</p>
-                        </div>
-                        <div className={`${style.message_in_dialog} ${style.message_for_me}`}>
-                            <div className={style.author_thumb}>
-                                <img src="https://st.fl.ru/users/he/heikun/foto/f_293609fee65839c2.jpeg" alt="author" />
-                            </div>
-                            <p>James, can you call me at Friday?</p>
-                        </div>
-                        <div className={`${style.message_in_dialog} ${style.message_for_people}`}>
-                            <div className={style.author_thumb}>
-                                <img src="https://st.fl.ru/users/he/heikun/foto/f_293609fee65839c2.jpeg" alt="author" />
-                            </div>
-                            <p>Yeah, I can call you at Friday on 6-7 p.m.</p>
-                        </div>
+                        {MessagesArray.map( message => <MessageItem userImage={message.userImage} textMessage={message.textMessage} messageStyle={message.messageStyle} /> )}
                     </Route>
                 </div>
             </div>
--- social/src/components/Profile/Posts/Posts.jsx
+++ social/src/components/Profile/Posts/Posts.jsx
@@ -2,12 +2,27 @@ import style from "./Posts.module.css";
 import React from "react";
 import Post from "./Post/Post";
 
+let PostArray = [
+    {
+        postId: 1,
+        postH1: 'My travel story',
+        postText: 'Hi',
+        likeCount: '22'
+    },
+    {
+        postId: 1,
+        postH1: 'My workout in Belarus',
+        postText: 'It\'s my first post',
+        likeCount: '13'
+    }
+]
+
 const Posts = () => {
+
     return (
         <div>
             <div className={style.profile__posts}>
-                <Post name="My travel story" message="Hi" likeCount="20" />
-                <Post name="My workout in Belarus" message="It's my first post" likeCount="13" />
+                {PostArray.map( post => <Post name={post.postH1} message={post.postText} likeCount={post.likeCount} /> )}
             </div>
             <form className={style.comment_form}>
                 <div className={style.comment_form__text}>
