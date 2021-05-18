421f9c9364a0f22c6aeb0a731e9c70fe89cf8baa
heikun <heikun@yandex.ru>
2021-05-18 18:48:47 +0300

    Add spotify player

--- social/src/components/Music/Music.jsx
+++ social/src/components/Music/Music.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import style from './Music.module.css';
+import SpotifyPlayer from 'react-spotify-player';
+
+const size = {
+    width: '100%',
+    height: 700,
+};
+const view = 'list'; // or 'coverart'
+const theme = 'black'; // or 'white'
 
 const Music = () => {
     return (
         <div className={style.container}>
             <div className={style.content}>
-                <h3>Music</h3>
+                <div className={style.music_from}>
+                    <SpotifyPlayer
+                        uri="spotify:album:1TIUsv8qmYLpBEhvmBmyBk"
+                        size={size}
+                        view={view}
+                        theme={theme}
+                    />
+                </div>
             </div>
         </div>
     )
