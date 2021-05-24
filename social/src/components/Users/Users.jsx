7945247016b6f72495c6a0e449096b53312b9eb8
heikun <heikun@yandex.ru>
2021-05-24 21:30:29 +0300

    Use componentDidMount

--- social/src/components/Users/Users.jsx
+++ social/src/components/Users/Users.jsx
@@ -4,8 +4,7 @@ import * as axios from "axios";
 import notFoundPhoto from "../../assets/images/not_found_photo.svg";
 
 class Users extends React.Component{
-     constructor(props) {
-         super(props);
+     componentDidMount() {
          axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
              this.props.setUsers(response.data.items)
          })
