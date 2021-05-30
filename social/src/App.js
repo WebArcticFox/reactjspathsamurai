b4dce950a6c5f659319ed732b256cb2ad65b447f
heikun <heikun@yandex.ru>
2021-05-30 21:09:26 +0300

    Use React.lazy() and Suspense

--- social/src/App.js
+++ social/src/App.js
@@ -1,9 +1,7 @@
-import React from "react";
+import React, {Suspense} from "react";
 import './App.css';
 import Footer from "./components/Footer/Footer";
 import HeaderContainer from "./components/Header/HeaderContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
-import MessagesContainer from "./components/Messages/MessagesContainer";
 import SidebarContainer from "./components/Sidebar/SidebarContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import News from "./components/News/News";
@@ -16,6 +14,8 @@ import Preloader from "./components/common/Prealoader/Preloader";
 import {initializeApp} from "./redux/appReducer";
 import {compose} from "redux";
 
+const MessagesContainer = React.lazy(() => import("./components/Messages/MessagesContainer"));
+const ProfileContainer = React.lazy(()=>import("./components/Profile/ProfileContainer"));
 
 class App extends React.Component {
     componentDidMount() {
@@ -34,9 +34,13 @@ class App extends React.Component {
             <div className="App">
                 <SidebarContainer/>
                 <HeaderContainer/>
-                <Route path="/profile/:userId?" render={() => <ProfileContainer/>}/>
+                <Suspense fallback={<Preloader />}>
+                    <Route path="/profile/:userId?" render={() => <ProfileContainer/>}/>
+                </Suspense>
                 <Route path="/users" render={() => <UsersContainer/>}/>
-                <Route path="/messages" render={() => <MessagesContainer/>}/>
+                <Suspense fallback={<Preloader />}>
+                    <Route path="/messages" render={() => <MessagesContainer/>}/>
+                </Suspense>
                 <Route path="/news" component={News}/>
                 <Route path="/music" component={Music}/>
                 <Route path="/settings" component={Settings}/>
